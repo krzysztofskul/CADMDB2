429627cf9953454118c803fa68856078b7db9938
krzysztofskul <krzysztofskul@protonmail.com>
2024-10-26 22:19:23 +0200

    Initial commit;

---
+++ src/main/java/pl/krzysztofskul/cadmdb2/Cadmdb2Application.java
@@ -0,0 +1,13 @@
+package pl.krzysztofskul.cadmdb2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cadmdb2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cadmdb2Application.class, args);
+	}
+
+}
